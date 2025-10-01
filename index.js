c106dad85434097c478c253234c9c6a4b71090b1
Aleksander <aleksander.tapsi@voco.ee>
2025-10-01 11:37:04 +0300

    Create users table and relation to products table data

--- index.js
+++ index.js
@@ -5,21 +5,41 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const sequelize = require("./util/db");
+const models = require("./models/index");
+sequelize.models = models;
+
+app.use((req,res,next) =>{
+  models.User.findByPk(1)
+  .then(user=>{
+    req.user = user
+    next()
+  })
+  .catch(err=> console.log(err))
+})
+
 const productAdminRoutes = require('./routes/admin/product')
 app.use('/admin', productAdminRoutes)
 
 const productRoutes = require('./routes/product')
 app.use(productRoutes)
 
-const sequelize = require("./util/db");
-const models = require("./models/index");
-sequelize.models = models;
+
 
 sequelize
   .sync()
   .then(() => {
-    console.log("Tabelid loodud");
-    app.listen(3002);
+    return models.User.findByPk(1)
+  })
+  .then(user =>{
+    if(!user){
+      return models.User.create({name: 'user', email:'user@local.com'})
+    }
+    return user
+  })
+  .then((user)=>{
+    console.log(user)
+    app.listen(3002)
   })
   .catch((error) => console.log(error));
 
